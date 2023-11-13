7d44f49d03da12e01168bca2f4a7039cb99d673d
NatthapongJit <66056023@kmitl.ac.th>
2023-11-13 22:31:14 +0700

    add (map.tab)

--- pages/tab.py
+++ pages/tab.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 st.set_page_config(layout='wide')
 
 st.markdown('สวัสดี! **Streamlit**')
-st.title('Layout and Decoration')
+st.title('Tab layout')
 st.write("""
 เราจะลองทำ San Francisco Dataset กันดู
 """)
@@ -26,7 +26,8 @@ if owners:
 df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
 df_dbh_grouped.columns = ['tree_count']
 
-col1, col2, col3 = st.columns(3)
+
+col1, col2, col3 = st.tabs(['line chart', 'bar chart', 'area chart'])
 with col1:
     st.line_chart(df_dbh_grouped)
 with col2:
@@ -38,21 +39,3 @@ st.title('แปลผล')
 st.write("""
 ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศูนย์กลาง 3' (2721 ต้น)
 """)
-
-st.divider()
-
-# trees_df = pd.read_csv('trees.csv')
-# df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
-# df_dbh_grouped.columns = ['tree_count']
-#
-# col1, col2, col3 = st.tabs(['line chart', 'bar chart', 'area chart'])
-# with col1:
-#     st.line_chart(df_dbh_grouped)
-# with col2:
-#     st.bar_chart(df_dbh_grouped)
-# with col3:
-#     st.area_chart(df_dbh_grouped)
-
-trees_df = trees_df.dropna(subset=['longitude','latitude'])
-trees_df = trees_df.sample(n=1000, replace=True)
-st.map(trees_df)
