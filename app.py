fe9c0be83bc55323ce600af6a192b30202e67050
NatthapongJit <66056023@kmitl.ac.th>
2023-11-13 22:13:26 +0700

    add page(map)

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# --- 01
-# https://docs.streamlit.io/library/api-reference/write-magic
+st.set_page_config(layout='wide')
+
 st.markdown('สวัสดี! **Streamlit**')
 st.title('Layout and Decoration')
 st.write("""
@@ -12,6 +12,17 @@ st.write("""
 """)
 
 trees_df = pd.read_csv('trees.csv')
+df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
+df_dbh_grouped.columns = ['tree_count']
+
+owners = st.sidebar.multiselect(
+    "Tree Owner Filter",
+    trees_df['caretaker'].unique()
+)
+
+if owners:
+    trees_df = trees_df[trees_df['caretaker'].isin(owners)]
+
 df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
 df_dbh_grouped.columns = ['tree_count']
 
@@ -23,21 +34,25 @@ with col2:
 with col3:
     st.area_chart(df_dbh_grouped)
 
-# trees_df = pd.read_csv('trees.csv')
-# df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
-# df_dbh_grouped.columns = ['tree_count']
-# st.line_chart(df_dbh_grouped)
-
 st.title('แปลผล')
 st.write("""
 ส่วนใหญ่ของต้นไม้ใน SF มีเส้นผ่าศูนย์กลาง 3' (2721 ต้น)
 """)
-# st.bar_chart(df_dbh_grouped)
-# st.area_chart(df_dbh_grouped)
 
-# st.write('จากโค้ด', '`st.markdown("สวัสดี!")`')
-# st.write(pd.DataFrame({
-#     'first column': [1, 2, 3, 4],
-#     'second column': [10, 20, 30, 40],
-# }))
-# st.divider()
+st.divider()
+
+# trees_df = pd.read_csv('trees.csv')
+# df_dbh_grouped = pd.DataFrame(trees_df.groupby(['dbh']).count()['tree_id'])
+# df_dbh_grouped.columns = ['tree_count']
+#
+# col1, col2, col3 = st.tabs(['line chart', 'bar chart', 'area chart'])
+# with col1:
+#     st.line_chart(df_dbh_grouped)
+# with col2:
+#     st.bar_chart(df_dbh_grouped)
+# with col3:
+#     st.area_chart(df_dbh_grouped)
+
+trees_df = trees_df.dropna(subset=['longitude','latitude'])
+trees_df = trees_df.sample(n=1000, replace=True)
+st.map(trees_df)
